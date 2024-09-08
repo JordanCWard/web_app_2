c9497311d6c903c74082f765367790c8782580a3
Jordan Ward <jordancward@gmail.com>
2024-09-08 10:38:59 -0600

    Added description, images and links

--- main.py
+++ main.py
@@ -28,8 +28,11 @@ info_about_apps = """
     """
 st.write(info_about_apps)
 
-# create two more columns
-column_b1, column_b2 = st.columns(2)
+"""
+create three columns, one on the left and one on the right, then one in the middle for space
+the list is for determining the size of the columns
+"""
+column_b1, empty_column, column_b2 = st.columns([1.5, 0.5, 1.5])
 
 
 data_file = pandas.read_csv("data.csv", sep=";")
@@ -37,10 +40,40 @@ data_file = pandas.read_csv("data.csv", sep=";")
 
 with column_b1:
     for index, row in data_file[:10].iterrows():
+
+        # reads the csv file and prints it out using the column given
         st.header(row["title"])
+        st.write(row["description"])
+
+        """
+        the pics are in the images folder in this directory,
+        then we add the column from the csv file to create the full file name
+        """
+        st.image("images/" + row["image"])
+
+        # how to create a link
+        # st.write("[Source Code](https://pythonhow.com")
+
+        # this adds links from the csv file in the url column
+        st.write(f"[Source Code]({row['url']})")
 
 
 with column_b2:
     for index, row in data_file[10:].iterrows():
+
+        # reads the csv file and prints it out using the column given
         st.header(row["title"])
+        st.write(row["description"])
+
+        """
+        the pics are in the images folder in this directory,
+        then we add the column from the csv file to create the full file name  
+        """
+        st.image("images/" + row["image"])
+
+        # how to create a link
+        # st.write("[Source Code](https://pythonhow.com")
+
+        # this adds links from the csv file in the url column
+        st.write(f"[Source Code]({row['url']})")
 
