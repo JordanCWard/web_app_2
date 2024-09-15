5f184533a5eebb0841c9257276449a76c352273c
Jordan Ward <jordancward@gmail.com>
2024-09-15 17:00:17 -0600

    Modify contact me page

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("I was pressed!")
